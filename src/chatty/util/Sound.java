325ec15e90883db922a584b45c48dee6ae1d77f9
GhettoGirl <lena.stoffler@ymail.com>
2016-03-27 19:40:43 +0200

    Fix Clip not closing after playback in util/Sound.java
    Close the Clip after playback has finished to prevent like one million "ALSA plug-in [java]" entries in the ALSA and/or PulseAudio mixer under Linux and other UNIX-like Operating Systems.
    
    The fix was very simple.
    I just added a LineListener to the Clip.

--- src/chatty/util/Sound.java
+++ src/chatty/util/Sound.java
@@ -10,24 +10,24 @@ import javax.sound.sampled.*;
 
 /**
  * Static methods to play sounds.
- * 
+ *
  * @author tduva
  */
 public class Sound {
-    
+
     private static final int MAX_VOLUME = 100;
     private static final int MIN_VOLUME = 0;
-    
+
     private static final int MIN_GAIN = -40;
-    
+
     private static final Logger LOGGER = Logger.getLogger(Sound.class.getName());
     private static final String PATH = System.getProperty("user.dir")
             +File.separator+"sounds"+File.separator;
-    
+
     private static final Map<String, Long> lastPlayed = new HashMap<>();
-    
+
     public static void play(String fileName, float volume, String id, int delay) {
-        
+
         if (lastPlayed.containsKey(id)) {
             long timePassed = (System.currentTimeMillis() - lastPlayed.get(id)) / 1000;
             if (timePassed < delay) {
@@ -38,16 +38,16 @@ public class Sound {
         if (delay >= 0) {
             lastPlayed.put(id, System.currentTimeMillis());
         }
-        
+
         try {
             // getAudioInputStream() also accepts a File or InputStream
             File file = new File(PATH+fileName);
             AudioInputStream ais = AudioSystem.getAudioInputStream(file);
-            
+
             DataLine.Info info = new DataLine.Info(Clip.class, ais.getFormat());
             Clip clip = (Clip)AudioSystem.getLine(info);
             clip.open(ais);
-            
+
             // Volume, use what is available
             String volumeInfo;
             FloatControl gain = getFirstAvailableControl(clip,
@@ -59,16 +59,23 @@ public class Sound {
             } else {
                 volumeInfo = "no volume control";
             }
-            
+
+            clip.addLineListener(new LineListener() {
+                public void update(LineEvent event) {
+                    if (event.getType() == LineEvent.Type.STOP)
+                        clip.close();
+                }
+            });
+
             clip.start();
             LOGGER.info("Playing sound "+id+"/"+fileName+" ("+volumeInfo+")");
         } catch (NullPointerException | LineUnavailableException | IOException | UnsupportedAudioFileException ex) {
             LOGGER.warning("Couldn't play sound ("+id+"/"+fileName+"): "+ex.getLocalizedMessage());
         }
     }
-    
+
     private static FloatControl getFirstAvailableControl(Clip clip,
-            FloatControl.Type... types) {
+                                                         FloatControl.Type... types) {
         for (FloatControl.Type type : types) {
             if (clip.isControlSupported(type)) {
                 return (FloatControl)clip.getControl(type);
@@ -76,7 +83,7 @@ public class Sound {
         }
         return null;
     }
-    
+
     private static float calculateGain(float volume, float min, float max) {
         // Restrict to min/max
         if (volume > MAX_VOLUME) {
@@ -86,7 +93,7 @@ public class Sound {
             volume = MIN_VOLUME;
         }
         volume = (float)(MAX_VOLUME - MAX_VOLUME*Math.pow(1.25, -0.25*volume));
-        
+
         if (min < MIN_GAIN) {
             min = MIN_GAIN;
         }
@@ -94,5 +101,5 @@ public class Sound {
         float gain = ((range * volume / (MAX_VOLUME - MIN_VOLUME)) + min);
         return gain;
     }
-    
+
 }
