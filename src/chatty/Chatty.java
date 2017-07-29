82e35a7e45f7deaa66a6b96bd8ae7ec20dc2a8b2
tduva <chattyclient@gmail.com>
2017-07-29 22:19:37 +0200

    v0.8.7b2

--- src/chatty/Chatty.java
+++ src/chatty/Chatty.java
@@ -54,7 +54,7 @@ public class Chatty {
      * by points. May contain a single "b" for beta versions, which are counted
      * as older (so 0.8.7b4 is older than 0.8.7).
      */
-    public static final String VERSION = "0.8.7b1";
+    public static final String VERSION = "0.8.7b2";
     
     /**
      * Enable Version Checker (if you compile and distribute this yourself, you
