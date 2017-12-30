18c369ec151e1e7c372250d3802b1fdd8d3e7639
magiruuvelvet <magiruuvelvet@naver.com>
2017-12-30 12:30:04 +0100

    Update version to 0.9.1b

--- src/chatty/Chatty.java
+++ src/chatty/Chatty.java
@@ -51,11 +51,11 @@ public class Chatty {
     public static final String REDIRECT_URI = "http://127.0.0.1:61324/token/";
     
     /**
-     * Version number of this version of Chatty, consisting of numbers seperated
+     * Version number of this version of Chatty, consisting of numbers separated
      * by points. May contain a single "b" for beta versions, which are counted
      * as older (so 0.8.7b4 is older than 0.8.7).
      */
-    public static final String VERSION = "0.9";
+    public static final String VERSION = "0.9.1b";
     
     /**
      * Enable Version Checker (if you compile and distribute this yourself, you
