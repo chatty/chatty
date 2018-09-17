f02ada1b1a87f37580472a3bb3fad31e3e3f616c
tduva <chattyclient@gmail.com>
2018-09-17 06:34:16 +0200

    Change icon position
    Change drawing to make better use of top spacings. Icons are shifted
    upwards by modifying the drawing Rectangle, then moved down by adjusting
    the alignment. This is kind of a hack, but then again changing the
    vertical span to change icon position and save on space was already kind
    of a hack. Eventually hopefully a similiar positioning can be achieved a
    bit nicer.

--- src/chatty/gui/components/textpane/MyIconView.java
+++ src/chatty/gui/components/textpane/MyIconView.java
@@ -2,10 +2,14 @@
 package chatty.gui.components.textpane;
 
 import chatty.util.Debugging;
+import java.awt.Graphics;
+import java.awt.Rectangle;
+import java.awt.Shape;
 import javax.swing.text.Element;
 import javax.swing.text.IconView;
 import javax.swing.text.StyleConstants;
 import javax.swing.text.View;
+import static javax.swing.text.View.Y_AXIS;
 
 /**
  * Changes the position of the icon slightly, so it overlaps with following text
@@ -15,7 +19,7 @@ import javax.swing.text.View;
  */
 class MyIconView extends IconView {
     
-    private static final int BOTTOM_MARGIN = 2;
+    private static final int MARGIN = 2;
     
     public MyIconView(Element elem) {
         super(elem);
@@ -23,7 +27,7 @@ class MyIconView extends IconView {
     
     @Override
     public float getAlignment(int axis) {
-        if (axis == View.Y_AXIS && Debugging.isEnabled("oldiconplace")) {
+        if (axis == View.Y_AXIS && Debugging.isEnabled("iconold1")) {
             return 1f;
         }
         if (axis == View.Y_AXIS) {
@@ -32,9 +36,10 @@ class MyIconView extends IconView {
             float actualHeight = super.getPreferredSpan(View.Y_AXIS);
             float fakedHeight = getPreferredSpan(View.Y_AXIS);
             float height = fakedHeight - (actualHeight - fakedHeight);
-            float shouldMoveUp = (height - lineHeight) / 2.0f;
-            float result = (fakedHeight - shouldMoveUp) / fakedHeight;
-//            System.out.println(height + " " + diff + " " + lineHeight + " " + result);
+            float moveDownBy = (height - lineHeight) / 2.0f;
+            moveDownBy += moveUpBy();
+            float result = (fakedHeight - moveDownBy) / fakedHeight;
+//            System.out.println(height + " " + moveDownBy + " " + lineHeight + " " + result);
             return result;
         }
         return super.getAlignment(axis);
@@ -42,40 +47,62 @@ class MyIconView extends IconView {
     
     @Override
     public float getPreferredSpan(int axis) {
-        if (axis == View.Y_AXIS && Debugging.isEnabled("oldiconplace")) {
+        if (axis == View.Y_AXIS && Debugging.isEnabled("iconold1")) {
             float height = super.getPreferredSpan(axis);
             return height * 0.7f;
         }
         if (axis == View.Y_AXIS) {
-//            int fontHeight = (int)getAttributes().getAttribute(Attribute.FONT_HEIGHT);
-//            float height = super.getPreferredSpan(axis);
-//            if (fontHeight >= height) {
-//                return height;
-//            }
-//            float bottomSpacing = StyleConstants.getLineSpacing(getAttributes()) * fontHeight + StyleConstants.getSpaceBelow(getAttributes());
-//            bottomSpacing = Math.max(bottomSpacing - 2, 0);
-//            float result = Math.max(fontHeight / height, 0.8f);
-//            float diff = height - result*height;
-//            return height - Math.min(diff, bottomSpacing);
-            
             int fontHeight = MyStyleConstants.getFontHeight(getAttributes());
             float height = super.getPreferredSpan(axis);
             if (fontHeight >= height) {
                 return height;
             }
-            float bottomSpacing = StyleConstants.getLineSpacing(getAttributes()) * fontHeight + StyleConstants.getSpaceBelow(getAttributes());
-            return height - Math.min(height - fontHeight, Math.max(bottomSpacing - BOTTOM_MARGIN, 0));
+            float spacing = StyleConstants.getLineSpacing(getAttributes()) * fontHeight + StyleConstants.getSpaceBelow(getAttributes());
+            if (!Debugging.isEnabled("iconold2")) {
+                spacing += Math.max(StyleConstants.getSpaceAbove(getAttributes()) - 1 - MARGIN, 0);
+            }
+            float toFontHeight = Math.max(height - fontHeight - MARGIN, 0);
+            float availableSpace = Math.max(spacing - MARGIN, 0);
+            System.out.println(toFontHeight+" "+availableSpace);
+            return height - Math.min(toFontHeight, availableSpace);
         }
         return super.getPreferredSpan(axis);
     }
     
-//    @Override
-//    public void paint(Graphics g, Shape a) {
-//        //System.out.println(a.getBounds()+" "+g.getClipBounds());
-//        Rectangle r = new Rectangle(a.getBounds());
-//        r.height = (int)super.getPreferredSpan(View.Y_AXIS);
-//        super.paint(g, r);
-//    }
+    private final Rectangle tempRect = new Rectangle();
+    
+    @Override
+    public void paint(Graphics g, Shape s) {
+        int moveUpBy = moveUpBy();
+        if (moveUpBy > 0) {
+            Rectangle r;
+            if (s instanceof Rectangle) {
+                r = (Rectangle) s;
+            } else {
+                r = s.getBounds();
+            }
+            tempRect.x = r.x;
+            tempRect.y = r.y;
+            tempRect.width = r.width;
+            tempRect.height = r.height;
+            tempRect.translate(0, -moveUpBy);
+            s = tempRect;
+        }
+        super.paint(g, s);
+    }
+    
+    private int moveUpBy() {
+        if (!Debugging.isEnabled("iconold2")) {
+            float height = super.getPreferredSpan(Y_AXIS);
+            float fakedHeight = getPreferredSpan(Y_AXIS);
+            int moveUp = ((int) (height - fakedHeight) / 2);
+            if (moveUp > 0) {
+                //return moveUp - 1;
+                return moveUp;
+            }
+        }
+        return 0;
+    }
     
     /**
      * Wrap Icon Labels as well, to prevent horizontal scrolling when a row of
