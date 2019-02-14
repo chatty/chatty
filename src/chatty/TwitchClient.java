1e46d63d30249c31d3dadf9328d8efead4a33d0f
tduva <chattyclient@gmail.com>
2019-02-14 13:32:21 +0100

    v0.9.5, cached debug time, update help/readme

--- src/chatty/TwitchClient.java
+++ src/chatty/TwitchClient.java
@@ -1966,7 +1966,7 @@ public class TwitchClient {
         }
         synchronized(cachedDebugMessages) {
             if (g == null) {
-                cachedDebugMessages.add(line);
+                cachedDebugMessages.add("["+DateTime.currentTimeExact()+"] "+line);
             } else {
                 if (!cachedDebugMessages.isEmpty()) {
                     g.printDebug("[Start of cached messages]");
@@ -1997,7 +1997,7 @@ public class TwitchClient {
     }
     
     /**
-     * Output a warning.
+     * Output a warning to the user, instead of the debug window.
      * 
      * @param line 
      */
