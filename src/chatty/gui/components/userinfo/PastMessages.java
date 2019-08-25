777e6ebd5d8cb10e69253932f5b74abdb823b2eb
tduva <chattyclient@gmail.com>
2019-08-25 14:35:13 +0200

    Fix past messages error

--- src/chatty/gui/components/userinfo/PastMessages.java
+++ src/chatty/gui/components/userinfo/PastMessages.java
@@ -143,7 +143,9 @@ public class PastMessages extends JTextArea {
             }
         }
         // Remove last newline
-        b.deleteCharAt(b.length() - 1);
+        if (b.length() > 0 && b.charAt(b.length() - 1) == '\n') {
+            b.deleteCharAt(b.length() - 1);
+        }
         return b.toString();
     }
     
